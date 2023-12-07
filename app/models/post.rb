dba17b852fb6b18df10b08f5c422306bd84f54da
Ismail mayito <ismayito1@gmail.com>
2023-12-07 18:58:46 +0300

    Fix review changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  # method that returns the 5 most recent comments for a given comment
-  scope :five_most_recent_posts_for_comment, lambda { |post_id|
-                                               where(id: post_id).order(created_at: :desc).limit(5)
-                                             }
-
   after_create :update_post_counter
   after_destroy :decrement_post_counter
+
+  # method that returns the 5 most recent comments for a given comment
+  def five_most_recent_posts_for_comment
+    posts.order('created_at Desc').limit(5)
+  end
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  scope :three_most_recent_posts_for_user, ->(user_id) { where(id: user_id).order(created_at: :desc).limit(3) }
-  has_many :posts
+  has_many :posts, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  def recent_post
+    posts.order('created_at Desc').limit(3)
+  end
 end
