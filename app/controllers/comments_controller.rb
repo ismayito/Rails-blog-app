5431941fe0168a1be465d87337ab1c9f524b4ee8
Ismail mayito <ismayito1@gmail.com>
2023-12-23 19:46:20 +0300

    Fix review changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
-  before_action :set_user_and_post
+  before_action :set_user_and_post, only: [:create]
 
   def index
     @comment = Comment.new
